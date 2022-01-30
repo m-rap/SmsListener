1da8750de662af546b1894061a2cf5fb3ead261f
rian <mohammad.rian.ap@gmail.com>
2022-01-31 00:55:16 +0700

    add sms content resolver

--- app/src/main/java/com/mrap/smslistener/SmsDb.java
+++ app/src/main/java/com/mrap/smslistener/SmsDb.java
@@ -1,10 +1,12 @@
 package com.mrap.smslistener;
 
+import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.Telephony;
 import android.util.Log;
 
 import java.text.SimpleDateFormat;
@@ -15,8 +17,10 @@ public class SmsDb {
     private SQLiteDatabase db = null;
     private static String TAG = "SmsDb";
     private SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd HH:mm:ss");
+    private Context context;
 
     public void openDb(Context context) {
+        this.context = context;
         SQLiteOpenHelper sqLiteOpenHelper = new SQLiteOpenHelper(context, context.getExternalFilesDir(null) + "/sms.db", null, 1) {
             @Override
             public void onCreate(SQLiteDatabase db) {
@@ -103,6 +107,50 @@ public class SmsDb {
 
         c.close();
         return res;
+    }
+
+    public void getSmssFromContentResolver(Context context) {
+        ContentResolver cr = context.getContentResolver();
+        Cursor c = cr.query(Telephony.Sms.CONTENT_URI, null, null, null, null);
+        int totalSMS = 0;
+        if (c == null) {
+            return;
+        }
+        totalSMS = c.getCount();
+        if (!c.moveToFirst()) {
+            c.close();
+            return;
+        }
+
+        int idxDate = c.getColumnIndexOrThrow(Telephony.Sms.DATE);
+        int idxAddr = c.getColumnIndexOrThrow(Telephony.Sms.ADDRESS);
+        int idxBody = c.getColumnIndexOrThrow(Telephony.Sms.BODY);
+        int idxType = c.getColumnIndexOrThrow(Telephony.Sms.TYPE);
+
+        for (int j = 0; j < totalSMS; j++) {
+            String smsDate = c.getString(idxDate);
+            String number = c.getString(idxAddr);
+            String body = c.getString(idxBody);
+            Date dateFormat = new Date(Long.valueOf(smsDate));
+            String type;
+            switch (Integer.parseInt(c.getString(idxType))) {
+                case Telephony.Sms.MESSAGE_TYPE_INBOX:
+                    type = "inbox";
+                    break;
+                case Telephony.Sms.MESSAGE_TYPE_SENT:
+                    type = "sent";
+                    break;
+                case Telephony.Sms.MESSAGE_TYPE_OUTBOX:
+                    type = "outbox";
+                    break;
+                default:
+                    break;
+            }
+
+
+            c.moveToNext();
+        }
+        c.close();
     }
 
     public ArrayList<String[]> getLastSmss() {
